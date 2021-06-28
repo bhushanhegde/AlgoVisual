26cc5b2569de6d611cca7e71b7a9280a2dbe3586
bhushanhegde <45070488+bhushanhegde@users.noreply.github.com>
2021-06-28 17:17:53 +0530

    updated

--- VisualSortAlgo.py
+++ VisualSortAlgo.py
@@ -1,6 +1,101 @@
 import pygame
 import random
 import time
+from time import sleep
+import VisualBubbleSort
+import VisualSelectionSort
+import VisualInsertionSort
+import VisualMergeSort
+import VisualQuickSort
+pygame.init()
+
+#colors
+white=(255,255,255)
+black=(0,0,0)
+blue=(0,0,255)
+red=(255,0,0)
+green=(0,255,0)
+yellow=(200,200,0)
+purple=(0,200,200)
+width=1200
+hight=600
+firtPage=pygame.display.set_mode((width,hight))
+pygame.display.set_caption("SORTING ALGORITHMS")
+
+firtPage.fill(black)
+
+pygame.display.update()
+f=pygame.font.SysFont('comicsansms',50)
+def rectMSG(msg,pos,color):
+    text=f.render(msg,True,color)
+    firtPage.blit(text,pos)
+def buttons():
+    pygame.draw.rect(firtPage,red,(50,30,300,100))
+    rectMSG("1.BubbleSort",[60,50],black)
+
+    pygame.draw.rect(firtPage,blue,(400,30,300,100))
+    rectMSG("2. SelectionSort",[410,50],black)
+    pygame.draw.rect(firtPage,yellow,(800,30,300,100))
+    rectMSG("3. MergeSort",[810,50],black)
+    pygame.draw.rect(firtPage,purple,(150,150,300,100))
+    rectMSG("4. InsertionSort",[160,170],black)
+    pygame.draw.rect(firtPage,white,(600,150,300,100))
+    rectMSG("5. QuickSort",[610,170],black)
+
+    rectMSG("Press the number of your choice i.e. 1",[200,400],white)
+    pygame.display.update()
+    
+    #
+    #cur=pygame.mouse.get_pos()
+    #click=pygame.mouse.get_pressed()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_1:
+                firtPage.fill(black)
+                pygame.display.update()
+                VisualBubbleSort.gameloop()
+                main()
+
+            if event.key==pygame.K_2:
+                firtPage.fill(black)
+                pygame.display.update()
+                VisualSelectionSort.gameloop()
+                main()
+
+            if event.key==pygame.K_3:
+                firtPage.fill(black)
+                pygame.display.update()
+                VisualMergeSort.gameloop()
+                main()
+            if event.key==pygame.K_4:
+                firtPage.fill(black)
+                pygame.display.update()
+                VisualInsertionSort.gameloop()
+                main()    
+            if event.key==pygame.K_5:
+                firtPage.fill(black)
+                pygame.display.update()
+                VisualQuickSort.gameloop()
+                main()
+
+    
+def main():
+    exit=False
+    while not exit:
+        buttons()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                exit=True
+        pygame.display.update()
+
+main()
+
+'''
+import pygame
+import random
+import time
 import VisualBubbleSort
 import VisualSelectionSort
 import VisualInsertionSort
@@ -86,3 +181,4 @@ def main():
         pygame.display.update()
 
 main()
+'''
